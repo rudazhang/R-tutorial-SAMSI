3fa891183a0bc548e7ce4d1f48fb6371c5ebf71c
ZHANG, Ruda <rudazhan@usc.edu>
2020-02-23 22:00:41 -0500

    Update tutorial.

--- 0-tutorial/tutorial.R
+++ 0-tutorial/tutorial.R
@@ -2,7 +2,9 @@
 ## author: Ruda Zhang <rzhang@samsi.info>
 ## date: 2020-02-24
 
-## Get help
+## Get help, you'll need this very often.
+## If you are in an R console, this prints the help info;
+## If you are in an IDE like RStudio, it shows in a separate window.
 ?help
 
 ## Operator ------------------------------
@@ -14,6 +16,7 @@
 3 / 2
 ## power
 3 ^ 2
+## Special operations in the form of `%.%`
 ## modulo
 5 %% 3
 ## integer division
@@ -36,28 +39,6 @@ TRUE | FALSE
 !TRUE
 
 
-## Function ------------------------------
-## "Everything that happens is a function call."
-## To call a function, type function name and parentheses,
-## where you may provide a comma-separated list of arguments.
-getwd()
-help("getwd")
-exp(1)
-sum(1, 2)
-
-## Define a function object: argument name, function body.
-square <- function(x) x^2
-square
-square(3)
-## In a compound statement, value of the the last express is returned.
-## Or you can explicitly call `return()`.
-square <- function(x) {
-    y <- x^2
-    return(y)
-}
-square(3)
-
-
 ## Object ------------------------------
 ## "Everything that exists is an object."
 ## pre-defined objects
@@ -66,8 +47,6 @@ letters
 LETTERS
 month.name
 month.abb
-## Functions are objects too.
-c
 ## Assignment creates new objects.
 x <- 1
 ## In an interactive session, an object by itself implicitly calls `print()`.
@@ -77,15 +56,67 @@ print(x)
 (x <- 1)
 print(x <- 1)
 
+
+## Function ------------------------------
+## "Everything that happens is a function call."
+## To call a function, type function name and parentheses.
+getwd()
+## Call help() or ? with an object name to search for help info.
+help("getwd")
+?getwd
+## You may provide an argument to functions.
+exp(1)
+## Or provide a comma-separated list of arguments.
+sum(1, 2)
+## Some functions have an indefinite number of arguments.
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+## Arguments are specified eith by position or by argument name.
+## For functions with many arguments, use argument name for clarity.
+print(pi, digits = 10)
+## You don't have to provide arguments with default values.
+
+## Functions are objects too.
+c
+## To define a function object, provide argument names and a function body.
+function(x) x^2
+## Call this function object as usual.
+(function(x) x^2)(3)
+## You can provide default values to an argument.
+(function(x = 3) x^2)()
+## To use the function later, assign it a name.
+square <- function(x) x^2
+square
+square(3)
+## In a compound statement, value of the the last expression is returned.
+## Or you can explicitly call `return()`,
+## especially when you want to exit before the end of function body.
+square <- function(x) {
+    y <- x^2
+    return(y)
+}
+square(3)
+
+
+## More on objects ------------------------------
 ## An object has three properties (metadata), besides its components.
 ## length
 length(letters)
 ## type
 typeof(letters)
-## attributes
-attributes(sleep)
+## attributes: name, class, etc.
+attributes(iris)
+names(iris)
+class(iris)
+## Many attributes have a replacement form
+## to simulates modification of mutable objects.
+## So you can change them like this:
+(x <- tolower(names(iris)))
+names(iris) <- x
+names(iris)
 ## To get an overview of an object:
-str(sleep)
+str(iris)
+## To view the first few elements of an object:
+head(iris)
 
 ## Atomic vectors (of length one)
 ## NULL
@@ -103,38 +134,44 @@ NaN
 ## complex
 1i
 ## character
-"alphabet"
+"apple"
 ## Check the atomic type of a vector
 typeof(1)
+typeof(1L)
 
 ## Atomic vectors of multiple elements
 ## 'c' is for concatenation.
 c(1, 2, 3, 4)
+## This is a (partially) named atomic vector.
+c(1, 2, apple = 3, 4)
 ## integer sequence
 1:4
 seq(from = 1, to = 4)
 
 ## Recursive vector: list
-(l <- list(1L, pi, "e"))
-l
+## A list is a collection of (any type of) vectors.
+## This is a named list of atomic vectors.
+(l <- list(a = 1L, b = pi, c= "e"))
+## This is a list of a list and a integer vector.
+list(l, seq(3))
 
-## Subsetting a vector
+## Subsetting a vector: brackets
 y <- c(1, 2, 3, 4)
-## a subset of one
+## a subset consisting of the first element only
 y[1]
-## all but one
+## all elements but the first
 y[-1]
-## two elements
+## the first and the fourth elements
 y[c(1, 4)]
-## exclude two elements
+## exclude the two elements
 y[-c(1, 4)]
 
 ## Access an element
-## access by index
+## access by index: double bracket
 l[[1]]
-## access by name
-names(l) <- c("a", "b", "c")
+## access by name: double bracket
 l[["a"]]
+## access by name: dollar sign
 l$a
 ## Sub-assignment: assign to specific element/subset
 l$a <- 4
@@ -146,39 +183,43 @@ l
 ## Classes are used to define new data types.
 
 ## Matrix
-A <- matrix(c(2, 1, 1, 5), nrow = 2)
+A <- matrix(c(2, 3, 1, 5), nrow = 2)
 A
 typeof(A)
 class(A)
 ## Subsetting a matrix
+## first row, first colum
 A[1, 1]
+## first colum
 A[, 1]
+## first row
 A[1, ]
 ## Linear algebra
 ## 't' is for transpose
 t(A)
-## matrix addition
-B <- matrix(c(2, 2, 3, 5), nrow = 2)
+## matrix addition is vector addtion
+(B <- matrix(c(2, 2, 3, 5), nrow = 2))
 A + B
 ## matrix multiplication
 A %*% B
-### elementwise multiplication
+### compare with elementwise/vector multiplication
 A * B
 ## solve a system of linear equations: Ax = b
-b <- c(1, 2)
+(A <- matrix(c(2, 1, 1, 5), nrow = 2))
+(b <- c(1, 2))
 solve(A, b)
 ## Cholesky decomposition
 (R <- chol(A))
-## Triangular solvers
+## Triangular solvers are faster.
 backsolve(R, backsolve(R, b, transpose = TRUE))
 
 ## Data frame
 ## "data.frame" is a named list of equal-length vectors,
 ## which is an elegant way of organizing related data.
-df <- data.frame(name = letters[1:3], number = 1:3)
+df <- data.frame(char = letters[1:3], number = 1:3)
 df
 class(df)
-df$name
+df$char
 df$number
 
 ## Package ------------------------------
@@ -188,6 +229,21 @@ install.packages(c("data.table", "purrr"))
 ## Load packages
 library(data.table)
 library(purrr)
+
+## access exported objects in a package: double colon
+## In an interactive R session, you don't need to specify the package name
+## once you've loaded the package by `library()`.
+DT <- data.table::as.data.table(iris)
+## data.table is a derived class of data.frame
+class(DT)
+## It has a nice `print()` method
+DT
+## And a much richer functionality
+DT[, .N, keyby = .(species)]
+
+## `purrr` provides pure functional programming in R.
+purrr::map(seq(3), square)
+
 
 ## Statistics ------------------------------
 ## Sampling
@@ -212,7 +268,7 @@ pnorm(0)
 ## Quantile function
 qnorm(0.5)
 
-## Statistical function
+## Statistical functions
 x <- rnorm(1000)
 summary(x)
 mean(x)
@@ -220,16 +276,25 @@ median(x)
 sd(x)
 
 ## Plot ------------------------------
-x <- seq(0, 2 * pi, length.out = 50)
-y <- sin(x)
+## data.table is useful for related data sets.
+(DT <- data.table(x = seq(0, 2 * pi, length.out = 10)))
+## create a new column
+DT[, y := sin(x)]
+DT
 ## basic plot
-plot(x, y)
-plot(x, y, type = "l")
-plot(x, y, type = "b", col = "red", lwd = 5)
+## points
+DT[, plot(x, y)]
+## line
+DT[, plot(x, y, type = "l")]
+## line and points; color; line width.
+DT[, plot(x, y, type = "b", col = "red", lwd = 5)]
+
+x <- rnorm(1000)
 ## histogram
-data <- rnorm(1000)
-hist(data, col = "gray")
-hist(data, breaks = 30, col = "gray")
+hist(x, col = "gray")
+hist(x, breaks = 30, col = "gray")
+## empirical cumulative distribution function
+plot.ecdf(x, pch = "")
 
 
 ## Statements ------------------------------
@@ -243,6 +308,8 @@ x <- 1 # Or append to any other line.
 x <- 1; print(x)
 
 ## Compound statement: statements grouped inside curly brackets `{}`.
+## Compound statements are used in function definition,
+## and usually in conditional statements and loopos.
 {
     x <- 1
     y <- 2
@@ -263,9 +330,6 @@ if (x > 0) {
 } else {
     -1
 }
-## elementwise conditional
-x <- -1:1
-ifelse(x > 0, TRUE, FALSE)
 ## choose from a list of alternatives
 x <- "a"
 switch(x, a = "apple", b = "banana", c = "cherry", "unknown")
@@ -281,12 +345,16 @@ while (i > 0) {
     print(i)
     i <- i - 1
 }
-## Functions as preferable alternatives to loops.
-## return an atomic vector of known type
+
+## Functions as preferable alternatives to conditionals and loops.
+## elementwise conditional
+x <- -1:1
+ifelse(x > 0, TRUE, FALSE)
+## loop over a vector: return an atomic vector of known type
 vapply(1:3, square, double(1))
-## return a list
+## loop over a vector: return a list
 lapply(1:3, square)
-## for side effects
+## loop over a vector for side effects
 purrr::walk(1:3, print)
 
 
